09fcacb20420fb41418d73f083b68aae30ff47aa
Amelia Siti Nur Azizah <63657315+4mel1a@users.noreply.github.com>
2020-07-24 11:59:53 +0700

    Add files via upload

---
+++ array.js
@@ -0,0 +1,24 @@
+// array string
+let jurusan = ["Rekayasa Perangkat Lunak", "Teknik Komputer dan Jaringan"]
+// array numerik
+let tingkatan_kelas = [10,11,12]
+// array objek
+let siswa = [
+    //objek pertama
+    {
+        nama : "Yaya", jurusan : "RPL"
+    },
+    //objek kedua
+    {
+        nama : "Ying", jurusan : "TKJ"
+    },
+    //objek ketiga
+    {
+        nama : "Gopal", jurusan : "RPL"
+    }
+]
+
+//Mendapatkan jumlah data yang trsimpan pada array
+console.log("Jumlah elemen array jurusan = " + jurusan.length);
+console.log("Jumlah elemen array tingkatan_kelas = " + tingkatan_kelas.length);
+console.log("Jumlah elemen array siswa = " + siswa.length);
